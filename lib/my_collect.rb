c02f06912c88deba319330b3c467eb0e33a708fb
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-06-18 14:06:58 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,3 +1,19 @@
-def my_collect(collection)
-  
+def my_collect(names)
+  i = 0 
+  collection = []
+  while i < names.length 
+  collection << yield(names[i])
+  i += 1 
+end 
+collection 
 end
+
+#def hello(array)
+# i = 0
+# collection = []
+# while i < array.length
+#   collection << yield(array[i])
+#   i += 1
+# end
+# collection
+#end
